b60b1159b5fe91f66bb5ca40ec9485965af9f293
Kassim <kassimrooney@gmail.com>
2025-07-23 16:43:55 +0300

    Added a Sign/login page and styled it

--- mealy-client/src/main.jsx
+++ mealy-client/src/main.jsx
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
+import Signlogin from "./pages/Signlogin";
 import Layout from "./components/Layout";
-import "./index.css";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index element={<Signlogin />} />
           {/* Add more routes here like <Route path="login" element={<Login />} /> */}
         </Route>
       </Routes>
