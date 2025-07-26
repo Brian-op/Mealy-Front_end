5366e46dbe5a06da9544333780307eb5e3f5c75b
lenny <lenny.kimanthi@student.moringaschool.com>
2025-07-26 16:42:48 +0300

    Final changes before branch switch

--- src/__tests__/Dashboard.test.js
+++ src/__tests__/Dashboard.test.js
@@ -2,12 +2,11 @@ import { render, screen } from "@testing-library/react";
 import Dashboard from "../pages/Dashboard";
 import { MemoryRouter } from "react-router-dom";
 
-test("renders dashboard", async () => {
+test("renders dashboard header", () => {
   render(
     <MemoryRouter>
       <Dashboard />
     </MemoryRouter>
   );
-
-  expect(screen.getByText(/today's menu/i)).toBeInTheDocument();
+  expect(screen.getByText(/Today's Menu/i)).toBeInTheDocument();
 });
