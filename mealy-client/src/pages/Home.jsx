d2ee9e63ea7644eaeed82feda01466039bfed7eb
Rodney <rodney.amani@student.moringaschool.com>
2025-07-27 13:56:30 +0300

    home pg created

--- mealy-client/src/pages/Home.jsx
+++ mealy-client/src/pages/Home.jsx
@@ -1,15 +1,74 @@
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Home.css';
+import { useAuth } from '../context/AuthContext'; 
+
+
 function Home() {
+  const { user } = useAuth();
   return (
-    // <div className="container">
-    //   <h1>Welcome to Mealy!</h1>
-    //   <p>Order your favorite meals with ease.</p>
-      
-    //   <div className="hero-buttons">
-    //     <a href="/signup" className="button-primary">Sign Up</a>
-    //     <a href="/login" className="button-secondary">Login</a>
-    //   </div>
-    // </div>
-    null
+   <div className="hero">
+  <div className="glass-container">
+    <div className="nav">
+      <div className="brand">MEA<i className="fa-solid fa-utensils"/>Y</div>
+      <div className='nav-right'>     <ul>
+        <li>
+          <NavLink to="/home" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/meals" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+            Menu
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/meals" className="order-now">
+            Order Now
+          </NavLink>
+        </li>
+        <li>
+        {user && user.role === "admin" && (
+              <NavLink
+                to="/admin"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
+                Admin Panel
+              </NavLink>
+            )}
+            </li>
+      </ul>
+      <div className="profile-icon">
+        <NavLink to="/profile" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+          <i className="fa-regular fa-circle-user"></i>
+        </NavLink>
+      </div>
+      </div>
+ 
+    </div>
+
+    <div className="hero-text">
+      <h1>MORE THAN FINGER<br />LICKING GOOD!</h1>
+      <p>Get the best quality and tasty meals</p>
+      <NavLink to="/meals" className="cta-btn">Order Food</NavLink>
+    </div>
+  </div>
+
+  <footer className="footer">
+    <div className="footer-left">
+      <i className="fa-brands fa-instagram"></i>
+      <i className="fa-brands fa-x-twitter"></i>
+      <p>&copy; {new Date().getFullYear()} Mealy. All rights reserved.</p>
+    </div>
+    <div className="footer-right">
+      <p>Contact Us <i className="fa-solid fa-phone"></i></p>
+      <p>Private Policy</p>
+    </div>
+  </footer>
+</div>
+
   );
 }
 
