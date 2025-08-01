ea6aaf7a138f19c8aa7ab2dbdd23ac7e174fd7d0
Young Monarch <brian.kiprono8@student.moringaschool.com>
2025-08-01 09:18:14 +0300

    Update api.js

--- mealy-client/src/api.js
+++ mealy-client/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://mealy-back-end.onrender.com', // or localhost
+  baseURL: 'https://mealy-back-end.onrender.com', 
   headers: {
     'Content-Type': 'application/json',
   },
