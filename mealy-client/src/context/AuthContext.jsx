33a33ec91cbbfa3686c5674e8d99483983f4dede
Kassim999999 <kassimrooney@gmail.com>
2025-08-01 10:44:09 +0300

    Update AuthContext.jsx

--- mealy-client/src/context/AuthContext.jsx
+++ mealy-client/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from 'react';
-import API, { attachToken } from '../api';
+import API from '../api';
 
 const AuthContext = createContext();
 
